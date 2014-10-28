382887c162f1cbc2dafc7aeb0d60c13a2485844b
Josh Jeong <jcjeong@gmail.com>
2014-10-28 12:16:52 -0700

    Adds geocoder and parses tweet location

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var httpAdapter = 'http'
+  , geocoderProvider = 'google';
+
 var express = require("express")
   , app = express()
   , http = require("http").createServer(app)
@@ -7,6 +10,7 @@ var express = require("express")
   , dotenv = require('dotenv')
   , Twit = require('twit')
   , util = require('util')
+  , geocoder = require('node-geocoder').getGeocoder(geocoderProvider, httpAdapter, extra)
   , mongoose = require('mongoose');
 
 dotenv.load();
@@ -17,6 +21,11 @@ var T = new Twit({
   , access_token: process.env.ACCESS_TOKEN
   , access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
+
+var extra = {
+  apiKey: process.env.GOOGLE_API_KEY,
+  formatter: null
+}
 
 var db = mongoose.connection;
 
@@ -69,14 +78,17 @@ app.get("/", function(request, response) {
 
 
 io.on("connection", function(socket){
-  var filter = ['webdeveloper', 'web developer', 'webdev']
+  // var filter = ['webdeveloper', 'web developer', 'webdev']
+  var filter = ['food']
     , stream = T.stream('statuses/filter', { track: filter } )
 
   stream.on('tweet', function (data) {
+
     var tweetText = data.text
       , screenName = data.user.screen_name
       , date = data.created_at
       , pic = data.user.profile_image_url
+      , hasCoord = data.coordinates
       , parameters = {
            screenName  : screenName,
            date        : date,
@@ -84,12 +96,25 @@ io.on("connection", function(socket){
            tweetText   : tweetText
       }
 
-    var streamTweet = new Tweet(parameters);
-    streamTweet.save(function (err) {
-      if (err)
-      console.log('bark');
-    });
-    io.sockets.emit('newTweet', {tweet: parameters})
+      if(hasCoord!=null){
+        coord = data.coordinates.coordinates
+        longitude = coord[0]
+        latitude = coord[1]
+        geocoder.reverse(latitude, longitude, function(err, res) {
+          console.log(res)
+        })
+        io.sockets.emit('newTweet', {tweet: parameters})
+      }
+      else{
+        // io.sockets.emit('newTweet', {tweet: parameters})
+      }
+    // });
+
+    // var streamTweet = new Tweet(parameters);
+    // streamTweet.save(function (err) {
+    //   if (err)
+    //   console.log('bark');
+    // });
   })
 })
 
--- public/js/index.js
+++ public/js/index.js
@@ -4,25 +4,23 @@ function init() {
     , socket = io.connect(serverBaseUrl);
 
   // socket.on('connect', function () {
-
+  
   // })
  
    socket.on('newTweet', function(data){
+
     var tweetText = data.tweet.tweetText
       , screenName = data.tweet.screenName
       , date = data.tweet.date
       , pic = data.tweet.pic
+      , coord = data.tweet.coord
 
 
-   if(tweetText.indexOf("") > -1){
-      $('#tweet-river').prepend("<a href='#' class='img-circle col-md-2 pic'><img src=" + pic + "></a><span class='username col-md-6'>" + screenName +"</span>  "+"<span class='time col-md-6'>"+ date+"</span><div class = 'tweet col-md-11'>"+tweetText+"</div>")
+    if(tweetText.indexOf("") > -1){
+      console.log(coord)
+      $('#tweet-river').prepend("<a href='#' class='img-circle col-md-1 pic inline'><img src=" + pic + "></a><span class='username col-md-6 inline'>" + screenName +"</span>"+"<span class='time col-md-6 inline'>"+ date+"</span><div class = 'tweet col-md-11 inline-block'>"+tweetText+"</div>")
     }
   })
  }
 
 $(document).on('ready', init);
-
-
-
-
-
