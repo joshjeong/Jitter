7637822ff9566afee5390d71c254d123db32890f
Josh Jeong <jcjeong@gmail.com>
2014-10-28 22:57:57 -0700

    Changes to app.js
    Extracts model from app.js
    Requires model file
    Adds tweet_id and loc to parameter

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ var express = require("express")
   , Twit = require('twit')
   , util = require('util')
   , geocoder = require('node-geocoder').getGeocoder(geocoderProvider, httpAdapter, extra)
+  , fs = require('fs')
   , mongoose = require('mongoose');
+
 
 dotenv.load();
 
@@ -29,14 +31,14 @@ var extra = {
 
 var db = mongoose.connection;
 
-var tweetSchema = mongoose.Schema({
-    screenName : String
-  , date       : Date
-  , pic        : String
-  , tweetText  : String
-});
+// fs.readdirSync(__dirname + '/models').forEach(function(filename){
+//   if(~filename.indexOf('.js')){
+//     var test = require(__dirname + '/models/' + filename)  
+//     console.log(__dirname + '/models/' + filename)
+//   } 
+// })
 
-var Tweet = mongoose.model('Tweet', tweetSchema);
+var tweetModel = require('./models/tweet.js')
 
 
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -56,6 +58,13 @@ app.get("/", function(request, response) {
   response.render("index");
 });
 
+app.get("/filter", function(req, res){
+  mongoose.model('Tweet').find(function(err,tweet){
+    res.send(tweet)
+  })
+})
+
+
 io.on("connection", function(socket){
   // var filter = ['webdeveloper', 'web developer', 'webdev']
   var filter = ['food']
@@ -64,35 +73,58 @@ io.on("connection", function(socket){
   stream.on('tweet', function (data) {
 
     var tweetText = data.text
+      , tweetId = data.id
       , screenName = data.user.screen_name
       , date = data.created_at
       , pic = data.user.profile_image_url
       , hasCoord = data.coordinates
+      , userLocation = data.user.location
       , parameters = {
-           screenName  : screenName,
-           date        : date,
-           pic         : pic,
-           tweetText   : tweetText
-      }
+           tweetId     : tweetId
+         , screenName  : screenName
+         , date        : date
+         , pic         : pic
+         , tweetText   : tweetText
+      };
 
       if(hasCoord!=null){
-        coord = data.coordinates.coordinates
-        longitude = coord[0]
-        latitude = coord[1]
-        geocoder.reverse(latitude, longitude, function(err, res) {
-          console.log(res)
-        })
-        io.sockets.emit('newTweet', {tweet: parameters})
-      }
-      else{
-        io.sockets.emit('newTweet', {tweet: parameters})
-      }
+        var coord = data.coordinates.coordinates
+          , longitude = coord.shift()
+          , latitude = coord.shift();
 
-    // var streamTweet = new Tweet(parameters);
-    // streamTweet.save(function (err) {
-    //   if (err)
-    //   console.log('bark');
-    // });
+        geocoder.reverse(latitude, longitude, function(err, res) {
+          var locInfo = res.shift()
+            , streamTweet = new tweetModel(parameters);
+
+          parameters.loc = {
+            country : locInfo.country,
+            city    : locInfo.city,
+            state   : locInfo.state
+          };
+          
+          // Tweet.findOne({ tweetId: tweetId }, function (err, doc) {
+          //   if (err){
+          //     console.log('error when saving')
+          //   }
+          //   console.log('this is doc')
+          //   doc = parameters;
+          //   console.log(doc)
+          //   doc.save(function(){});
+          // })
+
+
+          streamTweet.save(function (err) {
+          if (err)
+            console.log('bark');
+          });
+          
+          io.sockets.emit('newTweet', {tweet: parameters})
+        })
+      }
+      // else if(userLocation!=''){
+      //   console.log(userLocation)
+      // }
+
   })
 })
 
